f7e59c5e9e6868cebaa88a51ae968d62cc201dc2
kpy <kate@drkatewolin.com>
2013-10-21 13:08:02 -0500

    fixed missing comma

--- app/controllers/garage_controller.rb
+++ app/controllers/garage_controller.rb
@@ -3,10 +3,14 @@ class GarageController < ApplicationController
 def list
 
 @stuff = [
-  {item: 'Childrens Clothes', price: "$0.25 each"},
-  {item: 'Old Shoes', price: "$1.00 each"},
-  {item: 'Dull Knives', price: "$2.00 each"}
-  {item: 'Target storage bins', price: "0.50 each"}
+  {item: 'Childrens Clothes',
+    price: "$0.25 each"},
+  {item: 'Old Shoes',
+    price: "$1.00 each"},
+  {item: 'Dull Knives',
+    price: "$2.00 each"},
+  {item: 'Target storage bins',
+    price: "$0.50 each"}
 
 ]
 
