917b623d95fefc5ee664183b88950579e22ca020
kpy <kate@drkatewolin.com>
2013-10-21 13:06:55 -0500

    added to stuff list

--- app/controllers/garage_controller.rb
+++ app/controllers/garage_controller.rb
@@ -6,6 +6,7 @@ def list
   {item: 'Childrens Clothes', price: "$0.25 each"},
   {item: 'Old Shoes', price: "$1.00 each"},
   {item: 'Dull Knives', price: "$2.00 each"}
+  {item: 'Target storage bins', price: "0.50 each"}
 
 ]
 
